aeeb02f3ec1bf19c8b3ba8de94541aff8ac6d150
het07-patel <76025681+Het-07@users.noreply.github.com>
2025-06-03 16:11:15 -0300

    Fix import path for HomePage component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
